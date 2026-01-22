4f919885ee4c5e8d8142f7ed79964096d02fb696
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2026-01-21 18:41:02 -0700

    added array code

---
+++ src/goph547lab00/arrays.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def square_ones(n):
+    """Create an nxn array of ones.
+    Parameters
+    ----------
+    n : int
+    The size of the array.
+    Returns
+    -------
+    numpy.ndarray, shape=(n,n)
+    """
+    return np.ones((n,n))
+
